7c93254416b95a9602d7b1b9285ebe0dcf4ab650
brett-odu <bwarr011@odu.edu>
2022-01-20 11:25:20 -0500

    Add initial files

---
+++ practice/greeting.py
@@ -0,0 +1,19 @@
+"""
+Created on: 1/20/22
+Author:     Brett Warren
+Project:    cs411w
+File:       hello_gold_team.py
+
+
+"""
+
+
+def main():
+    gold_team = ["Brett"]
+
+    for name in gold_team:
+        print(f'Hello, welcome back ' + name + '!')
+
+
+if __name__ == '__main__':
+    main()
