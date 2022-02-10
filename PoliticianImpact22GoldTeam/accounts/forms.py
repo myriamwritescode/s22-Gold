fd06b22c4d3639e72931d2be74b4af8d79997212
Gabriela Gamez <ggame001@odu.edu>
2022-02-10 16:57:59 -0500

    Application Prototype
    user authentication and permissions settings using django

---
+++ PoliticianImpact22GoldTeam/accounts/forms.py
@@ -0,0 +1,27 @@
+'''
+this module define all custome forms use
+'''
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm #for customizing the user creation form here
+from django.contrib.auth.models import User #this is Django default user model
+from django import forms
+
+# this where our models form will sit
+
+from .models import *
+class CustomerForm(ModelForm):
+	class Meta:
+		model = Customer
+		fields = '__all__'
+		exclude = ['user']
+
+class CreateUserForm(UserCreationForm): # here were you can modify the default User table
+   #email = forms.EmailField() # add a new field email
+   #age = forms.IntegerField() # add a new field age
+   class Meta:
+     model = User
+     fields = ['username', 'email', 'password1', 'password2'] # fields = ["username", "email","age", "password1", "password2"]
+
+
+
+
---
+++ PoliticianImpact22GoldTeam/accounts/urls.py
@@ -0,0 +1,49 @@
+'''
+
+urlpatterns - Contain all file path for this application
+
+'''
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views # <---- reset password
+
+urlpatterns = [
+
+#------------------------------------User Registration and Login Authentication--------------------------------------------------  
+    path('register/', views.registerPage, name="register"),# <----dynamic path
+	path('login/', views.loginPage, name="login"),  
+	path('logout/', views.logoutUser, name="logout"),
+
+#------------------------------------web page that display the information from the database--------------------------------------
+    path('', views.home,name="home"),
+    path('profile/', views.userPage, name="user-page"),
+    path('compare/', views.comparePage, name="user-compare"),
+    path('value/', views.valuePage, name="user-value"),
+    path('value/learnMore', views.valuePagelearnmore, name="user-learn-more"),
+    path('account/', views.accountSettings, name="account"),
+    
+    #path('customer/', views.customer), # <----static path name
+    path('customer/<str:pk_test>/', views.customer,name="customer"),#dynamic display a paticualar customer
+
+#-----------------------------------entering deleting updating information from database-----------------------------------------
+  
+  
+#-------------------------------------password reset------------------------------------------------------------------------------  
+    path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),
+     name="reset_password"), # Submit email form 
+
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"), 
+        name="password_reset_done"),# Email sent success message  
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"), 
+     name="password_reset_confirm"),# Link to password Rest form in email 
+
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"), 
+        name="password_reset_complete"),# Password successfully changed message
+
+
+]
