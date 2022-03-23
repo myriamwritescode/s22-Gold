f6653819a047d44495b2644baf922d06008e8d10
myriam15 <50278091+myriam15@users.noreply.github.com>
2022-03-23 05:24:43 -0700

    working on capability to get roll numbers and deal with voice votes

--- API_scraping/BillsToCategories.py
+++ API_scraping/BillsToCategories.py
@@ -6,6 +6,7 @@
 
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 #------------------------------------------------------------------------------
@@ -23,29 +24,51 @@ soup = BeautifulSoup(html, "html.parser")                   # soup object
 
 #------------------------------------------------------------------------------
 
-# EXTRACT KEYS
+# EXTRACT H.R.
 
 span_tags_keys = soup.find_all('span', class_='result-heading')
-for key_tag in range(len(span_tags_keys)):
-    if key_tag % 2 == 0:
-        print(span_tags_keys[key_tag].contents[0].string)
+#for key_tag in range(len(span_tags_keys)):
+#    if key_tag % 2 == 0:
+        #print(span_tags_keys[key_tag].contents[0].string)
 
 
 #------------------------------------------------------------------------------
 
-# EXTRACT VALUES
+# EXTRACT COMMITTEE
 
 span_tags_values = soup.find_all('span', class_='result-item')
 
-for value_tag in span_tags_values:
-    if value_tag.contents[2].string is not None:
-        if value_tag.contents[2].string.strip() != '':
-            print(value_tag.sourceline, value_tag.contents[
-                2].string.strip())
+#for value_tag in span_tags_values:
+#    if value_tag.contents[2].string is not None:
+#        if value_tag.contents[2].string.strip() != '':
+            #print(value_tag.contents[2].string.strip())
 
 
 #------------------------------------------------------------------------------
 
-# CREATE DICTIONARY
+# EXTRACT ROLL, HOUSE AND SENATE
 
-# TBD
+tag_list = []
+tags = soup.find_all('div', class_='sol-step-info')
+
+for i in range(len(tags)):
+    matchstr = re.search(r'no\.\s\d+\)\.', tags[i].contents[0])
+    matchvoice = re.search(r'voice\svote', tags[i].contents[0])
+    if matchstr:
+        if len(matchstr.group(0)) == 9:
+            print("3 digits")
+        if len(matchstr.group(0)) == 8:
+            print("2 digits")
+    if matchvoice:
+        print("Voice Vote")
+
+
+#tag = str(tags[1].contents[0])
+#matchstr = re.search(r'no\.\s\d+\)\.', tag).group(0)
+
+#print(type(tag))
+
+#if len(matchstr) == 9:
+#    print(matchstr[4:7])
+#if len(matchstr) == 8:
+#   print(matchstr[4:6])
