033e6640248fa21e80b552850711bfd0a106dcee
cameronbrader97 <cbrad007@odu.edu>
2022-04-20 03:17:48 -0400

    Added method for preforming operations on various models via SQL

---
+++ API_site/db_Multi_Merge_example.py
@@ -0,0 +1,51 @@
+#Author Cameron Brader 
+#from django.db import models
+#from django.contrib.auth.models import User
+#from PoliticianImpact.models import TestElectedOfficial
+
+
+ 
+# Connect to SQLite database
+#conn = sqlite3.connect(r'C:\Users\Cameron\OneDrive\Desktop\S@@-Gold\s22-Gold\API_site\db.sqlite3')
+  
+# Create cursor object
+#cursor = conn.cursor()
+  
+# Query for INNER JOIN
+#sql = '''SELECT bioguide_id, StudentName, AdvisorName 
+#FROM TestElectedOfficial 
+#INNER JOIN TestVote
+#ON TestElectedOfficial.bioguide_id = TestVote.Voter_id;'''
+  
+# Executing the query
+#cursor.execute(sql)
+  
+# Fetching rows from the result table
+#result = cursor.fetchall()
+#for row in result:
+# print(row)
+  
+# Closing the connection
+#conn.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
